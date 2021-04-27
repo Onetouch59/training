d08153f3ee6c09c267c3fc23c19c6d95d3fb8a16
acs <a.bederiat@codeur.online>
2021-04-27 11:43:17 +0200

    exo js

---
+++ 21_04_js/main.js
@@ -0,0 +1,20 @@
+const coucou = 55;
+console.log (coucou);
+
+function test(){
+    let hello = 24;
+    console.log(hello);
+
+}
+
+document.getElementById('button').addEventListener('click',test);
+let hello = 'salut nico aka le bg'
+console.log(hello);
+
+function truc(){
+    let hello = 32;
+    console.log(hello);
+
+}
+
+document.getElementById('click').addEventListener('click',truc);
---
+++ 26_04_js/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+     let utilisateur = {
+         nom: ['alan', 'turing'],
+         age: 29,
+         bonjour: function (){
+             alert('bonjour, je suis' + this.mom[o] + 'j\'ai'+this.age + 'ans');
+         }
+     }
+     console.log(typeof utilisateur);
+    
+    </script>
+</body>
+</html>
