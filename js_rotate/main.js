4b593cbd0efd8a019f42975094ae1166bd25d852
acs <a.bederiat@codeur.online>
2021-08-27 09:26:15 +0200

    collision

--- js_rotate/main.js
+++ js_rotate/main.js
@@ -1,17 +1,22 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    window.addEventListener("resize", detectOrientation) ;
-    detectOrientation() ;
-  });
-  
-  function detectOrientation(){
-    if (window.matchMedia("(orientation: portrait)").matches) {
-        rotateScreen.style.display='block';
-        displayContent.style.display='none'
-     }
-     
-     else { (window.matchMedia("(orientation: landscape)").matches) 
-     rotateScreen.style.display='none';
-     displayContent.style.display='block'
-     }
+  window.addEventListener("resize", detectOrientation) ;
+  detectOrientation() ;
+});
+
+function detectOrientation(){
+        console.log('detect');
+  if (window.matchMedia("(orientation: portrait)").matches) {
+        console.log('portrait');
+        rotateScreen.style.display = 'block';
+        displayContent.style.display = 'none';
+  }else{
+        console.log('landscape');
+        rotateScreen.style.display = 'none';
+        displayContent.style.display = 'block';
   }
-  
+}    
+
+document.addEventListener('DOMMouseScroll', functionScroll);
+function functionScroll() {
+  console.log('scroll');
+}
